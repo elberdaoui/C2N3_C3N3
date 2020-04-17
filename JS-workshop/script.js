bc408b248b14a16a5fd9c94ab563ef260ba830dd
elberdaoui <imad.elberdaoui@gmail.com>
2020-04-17 15:44:04 +0100

    script

---
+++ JS-workshop/script.js
@@ -0,0 +1,39 @@
+// var x = prompt("enter your name")
+// document.write("Hi " + x + " how are you");
+// // document.getElementById("test").innerHTML = ('Hi ' + x + ' how are you');
+// // console.log('Hi ' + x)
+// alert(typeof x)
+
+// var arr = ['soufiane', 20, 'youssoufia', true, 5.4545]
+//
+// // document.write(arr)
+// console.log(arr);
+//
+// // document.write(arr.join(' | '))
+//
+// // alert(arr.reverse());
+//
+// var narr = arr.slice();
+// console.log(narr);
+  var ar = ['a', 'b', 'c', 'd', 'm', 'q', 'g', 'h']
+
+  var arr2 = ['ahmed', 56667, false, 88.6788]
+
+  // var con = ar.concat(arr2)
+
+  // console.log(con)
+  // console.log(ar.copyWithin(3 ,4))
+  // console.log(ar.sort())
+  // var ar3 = ar.sort()
+  // console.log(ar3.reverse())
+// console.log(ar.pop())
+
+// var newAr = ar.slice(0, 4)
+// console.log(newAr)
+
+// ar.splice(4, 2, 'L')
+// alert(ar)
+
+// alert(ar.indexOf('m'))
+
+alert(ar.join(""))
